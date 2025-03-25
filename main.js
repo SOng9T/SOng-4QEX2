17e6ee7c4509a5f58000923035157f6b16d173c1
SOng9T <ong.sm@obmontessori.edu.ph>
2025-03-25 11:17:43 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// if statement
+function VerifyAge() {
+    let age = document.getElementById('inputAge').value; 
+    if (age >= 18) {
+        stmt 1;}
+        else {
+        stmt 2; 
+        }
+        window.alert("You're too young to vote"); 
+    }
+
